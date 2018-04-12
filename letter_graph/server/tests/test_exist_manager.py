5129d944489feaf4551fb5aa0c04f0bd8e400e17
Richard Hadden <oculardexterity@gmail.com>
2018-04-12 14:13:17 +0100

    switched tests to grappa

--- letter_graph/server/tests/test_exist_manager.py
+++ letter_graph/server/tests/test_exist_manager.py
@@ -1,5 +1,4 @@
-import inspect
-import logging
+from grappa import should
 import os.path
 import sys
 import unittest
@@ -62,16 +61,16 @@ class TestExistManagerClassMethods(unittest.TestCase):
 
     def test_configs_imported(self):
         for k, v in MOCK_EXIST_CONFIG['global'].items():
-            assert getattr(self.EM, k) == v
+            getattr(self.EM, k) | should.be.equal.to(v)
 
     def test_get_xqueries_from_exist(self):
-        assert self.EM._get_xqueries_from_exist() == ['test1', 'test2']
-        assert self.EM.xqueries == ['test1', 'test2']
+        self.EM._get_xqueries_from_exist() | should.be.equal.to(['test1', 'test2'])
+        self.EM.xqueries | should.be.equal.to(['test1', 'test2'])
 
     def test_build_xqueries_as_methods(self):
         exist = self.EM()
-        assert inspect.ismethod(exist.test1)
-        assert inspect.ismethod(exist.test2)
+        exist | should.implement.methods('test1', 'test2')
+
 
     def test_build_query_url(self):
         d = MOCK_EXIST_CONFIG['development']
@@ -80,24 +79,25 @@ class TestExistManagerClassMethods(unittest.TestCase):
         base_url = f"http://{d['address']}:{d['port']}/exist/apps/{g['app_name']}/test1.xql"
 
         # Test no args produced base url
-        assert self.EM._build_query_url('test1') == base_url
+        self.EM._build_query_url('test1') | should.be.equal.to(base_url)
 
         # Test single arg
-        assert self.EM._build_query_url('test1', thing='thang') == \
-            base_url + '?thing=thang'
+        self.EM._build_query_url('test1', thing='thang') | \
+            should.be.equal.to(base_url + '?thing=thang')
 
         # Test two args, one with spaces
-        assert self.EM._build_query_url('test1', thing='thang', other='one two') == \
-            base_url + '?thing=thang&other=one%20two'
+        self.EM._build_query_url('test1', thing='thang', other='one two') | \
+            should.be.equal.to(base_url + '?thing=thang&other=one%20two')
 
         # Test args passed as dict instead of keywords
-        assert self.EM._build_query_url('test1', {'thing': 'thang'}) == \
-            base_url + '?thing=thang'
+        self.EM._build_query_url('test1', {'thing': 'thang'}) | \
+            should.be.equal.to(base_url + '?thing=thang')
 
         # Test throw a warning if *args as dict and **kwargs both used 
         # (n.b. uses only *args dict in such cases)
         with warnings.catch_warnings(record=True) as ws:
             self.EM._build_query_url('test1', {'argKey': 'argValue'}, thing='thang')
-            assert str(ws[0].message) == 'Args passed as dict and keyword arguments; only args used'
+            str(ws[0].message) | \
+            should.be.equal.to('Args passed as dict and keyword arguments; only args used')
 
             
