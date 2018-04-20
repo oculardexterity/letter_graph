aac02e4fcb0a10271aadb6a5222cb0d1be47ca7f
Richard Hadden <oculardexterity@gmail.com>
2018-04-20 17:14:10 +0100

    built a graph class

--- letter_graph/server/letter_data_test.py
+++ letter_graph/server/letter_data_test.py
@@ -22,14 +22,11 @@ exist = ExistManager()
 graphml = exist.letters_base_graph_sync()
 
 
+
 class Graph(nx.Graph):
     
     @classmethod
-    def read_graphml(cls, file):
-        if type(file) == str:
-            file = StringIO(file)
-
-        baseObject = nx.read_graphml(file)
+    def create_instance(cls, baseObject):
         instance =  __class__()
         instance.__class__ = type(baseObject.__class__.__name__,
                               (instance.__class__, baseObject.__class__),
@@ -37,51 +34,56 @@ class Graph(nx.Graph):
         instance.__dict__ = baseObject.__dict__
         return instance
 
+    @classmethod
+    def from_graphml(cls, file):
+        if type(file) == str:
+            file = StringIO(file)
+
+        baseObject = nx.read_graphml(file)
+        return cls.create_instance(baseObject)
 
 
-def hash_graph(graph):
-    nodes_string = str(graph.nodes)
-    edges_string = str(graph.edges)
-    string_to_hash = ' '.join([nodes_string, edges_string])
-    graph_hash = hashlib.md5(string_to_hash.encode('utf-8')).hexdigest()
-    return graph_hash
+    def hash_graph(self):
+        nodes_string = str(self.nodes)
+        edges_string = str(self.edges)
+        string_to_hash = ' '.join([nodes_string, edges_string])
+        graph_hash = hashlib.md5(string_to_hash.encode('utf-8')).hexdigest()
+        return graph_hash
+
+    def get_positions(self):
+
+        graph_hash = self.hash_graph()
+        file_path = f'graph_positions/{graph_hash}.layout'
+        if os.path.isfile(file_path):
+            print(f'{graph_hash} found; loading')
+            with open(file_path, 'rb') as f:
+                return pickle.load(f)
+        else:
+            print(f'{graph_hash} not found; calculating')
+            positions = nx.spring_layout(self)
+            with open(file_path, 'wb') as f:
+                pickle.dump(positions, f)
+            return positions
+
+    def to_linkurious_json(self):
+        output = {"nodes": [], "edges": []}
+        positions = self.get_positions()
+        for key, data in self.nodes(data=True):
+            node = {}
+            node['data'] = data
+            node['id'] = key
+            node['x'], node['y'] = positions[key]
+            output["nodes"].append(node)
+
+        for source, target, data in self.edges(data=True):
+            edge = data
+            edge["source"] = source
+            edge["target"] = target
+            output["edges"].append(edge)
+
+        return output
 
 
-
-
-def build_graph_json(graph, positions):
-    output = {"nodes": [], "edges": []}
-    for key, data in graph.nodes(data=True):
-        node = {}
-        node['data'] = data
-        node['id'] = key
-        node['x'], node['y'] = positions[key]
-        output["nodes"].append(node)
-
-    for source, target, data in graph.edges(data=True):
-        edge = data
-        edge["source"] = source
-        edge["target"] = target
-        output["edges"].append(edge)
-
-    return output
-
-
-def get_positions(graph):
-
-
-    graph_hash = hash_graph(graph)
-    file_path = f'graph_positions/{graph_hash}.layout'
-    if os.path.isfile(file_path):
-        print(f'{graph_hash} found; loading')
-        with open(file_path, 'rb') as f:
-            return pickle.load(f)
-    else:
-        print(f'{graph_hash} not found; calculating')
-        positions = nx.spring_layout(graph)
-        with open(file_path, 'wb') as f:
-            pickle.dump(positions, f)
-        return positions
 
 '''
 graph = nx.read_graphml(StringIO(graphml))
